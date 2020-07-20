651973b4da654dd66c3bc7665c0e47b52d67b77c
ajay@neolen.com <ajay@neolen.com>
2020-07-20 14:27:10 +0530

    added menu animations

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
+import Menu from './comps/Menu'
 
 function App() {
+
+  const [open, setOpen] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="menu-icon" onClick={() => setOpen(!open)}>{open ? 'Close' : 'Menu'}</div>
+      {
+        open ? <Menu /> : null
+      }
+      <div className="main-pic">photo</div>
+      <div className="main-content">
+        <span className="main-content-title">
+          Hello{" "}
+          <span role="img" aria-label="waving hand">
+            👋
+          </span>
+          , <br /> I am Abhinav
+        </span>
+        <span className="main-content-sub">
+          4th year Undergraduate Student at <br /> Indian Institute of
+          technology, Kharagpur
+        </span>
+      </div>
+      <div className="btn-group">
+        <a href="#">Proposals &rarr;</a>
+        <a href="#">Suggest &rarr;</a>
+      </div>
     </div>
   );
 }
---
+++ src/comps/Menu.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+export default function Menu() {
+  const easing = [0.17, 0.37, 0.63, 0.47];
+
+  return (
+    <div className="menu">
+      <motion.div
+        initial={{ y: "-120%", skewY: 20 }}
+        animate={{
+          y: 0,
+          skewY: 0,
+          transition: { ease: easing, delay: 0, damping: 0 },
+        }}
+        className="menu-overlay-one"
+      ></motion.div>
+      <motion.div
+        initial={{ y: "-110%", skewY: 20 }}
+        animate={{
+          y: 0,
+          skewY: 0,
+          transition: {
+            ease: easing,
+            delay: 0.05,
+            damping: 0,
+          },
+        }}
+        className="menu-overlay-two"
+      ></motion.div>
+      <motion.div
+        initial={{ y: "-110%", skewY: 20 }}
+        animate={{
+          y: 0,
+          skewY: 0,
+          transition: { ease: easing, delay: 0.1, damping: 0 },
+        }}
+        className="menu-overlay-three"
+      ></motion.div>
+      <motion.div
+        initial={{ y: "-110%", skewY: 20 }}
+        animate={{
+          y: 0,
+          skewY: 0,
+          transition: {
+            ease: easing,
+            delay: 0.15,
+            damping: 0,
+          },
+        }}
+        className="menu-overlay-four"
+      ></motion.div>
+      <motion.div
+        initial={{ y: "-100%", skewY: 20 }}
+        animate={{
+          y: 0,
+          skewY: 0,
+          transition: {
+            ease: easing,
+            delay: 0.2,
+            damping: 0,
+          },
+        }}
+        className="menu-content"
+      >
+        <motion.div initial={{x: -100, opacity: 0}} animate={{x:0, opacity: 1, transition: {delay: 1}}} className="menu-content-social">
+          SDS 348, MMM Hall of Residence,
+          <br/> IIT Kharagpur, WB 721-302
+          <br/><br/>
+          ajajaj2807@gmail.com
+        </motion.div>
+        <motion.div initial={{x: -50, opacity: 0}} animate={{x:0, opacity: 1, transition: {duration: 0.5,delay: 0.8, staggerChildren: 1}}} className="menu-content-nav">
+          <motion.a href="#">About</motion.a>
+          <motion.a href="#">Credentials</motion.a>
+          <motion.a href="#">Contact</motion.a>
+          <motion.a href="#">FAQ</motion.a>
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+}
